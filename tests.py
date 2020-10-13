afe5563ba588249605a1032f5e8018702cf59d93
Stuffing <stuffingeu56@gmail.com>
2020-10-13 22:08:52 +0300

    test interfeis v0.00001

--- tests.py
+++ tests.py
@@ -1,4 +1,8 @@
-import datetime
+import  time
 
 
-print(str(datetime.datetime.day))
+def rerun(repeat, count):
+    for _ in range(count):
+        time.sleep(repeat * 60)
+        for _ in range(5):
+            return playsound(sound)
