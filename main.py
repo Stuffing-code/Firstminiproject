ac2c156ff315612e0c890ec865a9a7624edcb093
Stuffing <stuffingeu56@gmail.com>
2020-10-13 17:58:07 +0300

    finish v1.0

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ from playsound import playsound
 def rerun(time_repeat, count):
     for _ in range(count):
         time.sleep(time_repeat * 60)
-        return (f"Slept for {int(time_repeat)} "
-                f"min {int((time_repeat * 60) % 60)} second")
+        for _ in range(3):
+            return playsound(sound)
 
 
 def difference():
@@ -25,23 +25,34 @@ date_now = datetime.datetime.now()
 time_alarm = input("Введите день, месяц и точное время будильника: ").split()
 count_repeat = int(input("Сколько раз повторить: "))
 time_repeat = float(input("Через сколько минут повторить: "))
-data_difference = ((int(time_alarm[0]) - date_now.day) * 86400) + ((int(time_alarm[1]) - date_now.month) * 30 * 86400) \
+"""
+    Уменьшение нагрузки на программу расчет разныцы текущего времени и необходимого
+в секундах для отправки программы в ожидание.
+"""
+time_difference = ((int(time_alarm[0]) - date_now.day) * 86400)\
+                  + ((int(time_alarm[1]) - date_now.month) * 30 * 86400) \
                   + ((int(time_alarm[2]) - date_now.hour) * 3600) \
                   + ((int(time_alarm[3]) - date_now.minute) * 60) - date_now.second
-print(data_difference)
 while True:
-    if data_difference > 0:
-        print(f"До будильника осталось {data_difference // 60} минут {data_difference % 60} секунд")
-        time.sleep(data_difference)
+    if time_difference > 0:
+        print(f"До будильника осталось {time_difference // 60} минут {time_difference % 60} секунд")
+        time.sleep(time_difference)
+        """
+            Обновление времени текущего и расчет разницы времени
+        """
         date_now = datetime.datetime.now()
-        data_difference = ((int(time_alarm[0]) - date_now.day) * 86400) + (
+        time_difference = ((int(time_alarm[0]) - date_now.day) * 86400) + (
                 (int(time_alarm[1]) - date_now.month) * 30 * 86400) \
                           + ((int(time_alarm[2]) - date_now.hour) * 3600) \
                           + ((int(time_alarm[3]) - date_now.minute) * 60) - date_now.second
     else:
         if int(time_alarm[0]) == date_now.day and int(time_alarm[1]) == date_now.month \
                 and int(time_alarm[2]) == date_now.hour and int(time_alarm[3]) == date_now.minute:
+            """
+                Цикл для увеличение времени проигрывания музыки
+            """
             for i in range(1):
                 playsound(sound)
+            print(f"Будильник повториться через {int(time_repeat) * 60} секунд ")
             rerun(time_repeat, count_repeat)
         break
